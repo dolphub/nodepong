f08f7ec7ecd10165afafbb57c47e4d5a3fa861be
Randy Mikkelsaar <randy.mikkelsaar@gmail.com>
2017-04-17 00:10:58 -0400

    refactoring, updating project sturcture

---
+++ src/app/app.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import { Server } from 'http';
+import path from 'path';
+import 'colors';
+import morgan from 'morgan';
+
+const app = express();
+const http = Server(app);
+const io = require('socket.io')(http);
+
+import config from './config';
+
+// const nodePong = require('./pongServer');
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, './../../dist')));
+//
+// // Can get configuration from admin page
+// let ball = {
+// 	'delta_x': 1,
+// 	'delta_y': 1,
+// 	'fps': 15,
+// 	'radius': 20
+// };
+//
+// var pongServer = new nodePong(io, http, ball);
+//
+//
+// // Start server, start listeners
+// pongServer.initServer();
+// pongServer.startServer();
+
+// console.log(config);
+let listener = http.listen(config.PORT, config.HOST, () => {
+    // console.log(listener);
+    console.log(`Server listening on ${listener.address().address}:${listener.address().port}`.green);
+});
---
+++ src/app/config/development.js
@@ -0,0 +1,3 @@
+
+export const PORT = process.env.PORT || 3001;
+export const HOST = process.env.PORT || '127.0.0.1';
